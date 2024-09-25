21d20f82b610f0275fcb4b0b796dd287313b171a
MahshasdRoustapour <mahshadroustapour1@gmail.com>
2024-09-26 00:18:36 +0330

    feat: cd command added

--- commandline.py
+++ commandline.py
@@ -6,7 +6,7 @@ import os
 def setup():
     parser = argparse.ArgumentParser(description="CLI")
     parser.add_argument("command", help="ls, cd, mkdir, rmdir, rm, rm-r, cp, mv, find, cat, show_logs")  
-    parser.add_argument("path", nargs="?", default=".", help="Path for commands")
+    parser.add_argument("path", type=str, nargs="?", default=".", help="Path for commands")
     parser.add_argument("file", nargs="?", default=".", help="file for commands")
     parser.add_argument("directory", nargs="?", default=".", help="Directory to remove")
     parser.add_argument("source", nargs="?", help="Source for commands")
@@ -15,13 +15,15 @@ def setup():
     parser.add_argument("-r", nargs="?", help="Remove the directory at directory recursive")
     parser.add_argument("--show-logs",action="store_true", help="show all logs of the program")
     return parser
-
+def change_directory(path):
+    os.chdir(path)
+    print(f"directory changed to {path}")
 parser = setup()
 args = parser.parse_args()
 if args.command == "ls":
     pass
 elif args.command == "cd":
-    pass
+    change_directory(args.path)
 elif args.command == "mkdir":
     pass
 elif args.command == "rmdir":
